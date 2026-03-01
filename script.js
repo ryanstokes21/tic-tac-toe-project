86a49ac8a49b23bbda6fb93d4ee6375ef8b4d186
Ryan Stokes <ryanstokescfbre@gmail.com>
2026-02-28 22:20:47 -0600

    add logic to switch turns and update the board
    when a player selects a cell their turn will be
    over, the board will update, and it will then be
    player 2s turn

--- script.js
+++ script.js
@@ -8,3 +8,32 @@ function createPlayer(playerName, marker) {
 }
 const player1 = createPlayer('rob', 'x');
 const player2 = createPlayer('bill', 'o');
+
+function makeMove() {
+  let currentPlayer = player1;
+
+  return function switchTurn() {
+    const playerMove = +prompt('Enter a index between 0-8');
+    if(playerMove >= 0 && playerMove <= 9) {
+      if(currentPlayer){
+        gameBoard.board.forEach(() => {
+          gameBoard.board.splice(playerMove, 1, currentPlayer.marker);
+        })
+      }
+      currentPlayer = (currentPlayer === player1) ? player2 : player1;
+      updateBoard();
+    }
+  }
+
+}
+function updateBoard() {
+  console.log(`${player1.playerName}: ${player1.marker} | ${player2.playerName}: ${player2.marker}`)
+  console.log(`${gameBoard.board[0]} | ${gameBoard.board[1]} | ${gameBoard.board[2]}`);
+  console.log("---------");
+  console.log(`${gameBoard.board[3]} | ${gameBoard.board[4]} | ${gameBoard.board[5]}`);
+  console.log("---------");
+  console.log(`${gameBoard.board[6]} | ${gameBoard.board[7]} | ${gameBoard.board[8]}`);
+}
+
+switchTurn = makeMove();
+switchTurn();
