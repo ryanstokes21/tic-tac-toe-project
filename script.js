f990cb2b46996e0e7af73aeb409e37c50e57ac3b
Ryan Stokes <ryanstokescfbre@gmail.com>
2026-02-28 22:17:52 -0600

    change to single array
    single array aloud a easier way to get the index
    to change the value of the array item

--- script.js
+++ script.js
@@ -1,9 +1,5 @@
 const gameBoard = (() => {
-  const board = [
-                [1,2,3],
-                [4,5,6],
-                [7,8,9],
-                ];
+  const board = ['1','2','3','4','5','6','7','8','9'];
   return { board }
 })();
 
